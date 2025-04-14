c8fa4a88613903644f49452ae0d270d314201ee9
BrakhmaQA <mmarol9233@gmail.com>
2025-04-14 12:17:29 +0300

    WildBerries test task.

--- src/main/java/utils/ScreenshotHelper.java
+++ src/main/java/utils/ScreenshotHelper.java
@@ -1,17 +1,11 @@
 package utils;
 
 import io.qameta.allure.Allure;
-import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import ru.yandex.qatools.ashot.AShot;
-import ru.yandex.qatools.ashot.Screenshot;
 
-import javax.imageio.ImageIO;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.time.LocalDateTime;
 
 public class ScreenshotHelper {
